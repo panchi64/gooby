665ce13f3458560a71fa6e4a0cdee78e7ad21b49
Francisco Casiano <contact@franciscocasiano.com>
2021-12-28 17:18:19 -0400

    Restart time babyyyy
    The project has been nuked in order to start over with another library (Discord4J)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-         * TODO: Add storage for chats
-         * TODO: Add volatile storage for:
-         *              - usernames
-         *              - commands (saved until command is complete?)
-         *              - channels
-         *              - emojis
-         */
-//        Initialize a Discord API connection
-        api.discord.Main.init();
     }
 }
---
+++ src/main/java/api/discord/Categorizer.java
@@ -0,0 +1,5 @@
+package api.discord;
+
+// This class is to categorize messages depending on what they contain. (ie commands, @, textual triggers, etc)
+public class Categorizer {
+}
