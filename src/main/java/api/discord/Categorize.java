bbe4666db291c35d462d1705fc2578ab72541fea
Francisco Casiano <contact@franciscocasiano.com>
2022-01-28 16:48:12 -0400

    Updated dependencies and structured project

---
+++ src/main/java/api/discord/Categorize.java
@@ -0,0 +1,22 @@
+package api.discord;
+
+import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.event.domain.message.MessageUpdateEvent;
+import reactor.core.publisher.Mono;
+
+// This class is to categorize messages depending on what they contain. (ie commands, @, textual triggers, etc)
+public class Categorize {
+    //    Receives an event and depending on what type of event it is, send it to the corresponding command
+    public static Mono<Object> categorizeEvent(Event event) {
+        if (event instanceof ReadyEvent readyEvent) {
+            return Mono.empty();
+        } else if (event instanceof MessageCreateEvent messageCreateEvent) {
+            return Mono.empty();
+        } else if (event instanceof MessageUpdateEvent messageUpdateEvent) {
+            return Mono.empty();
+        } else
+            return Mono.empty();
+    }
+}
--- src/main/java/api/discord/Main.java
+++ src/main/java/api/discord/Main.java
@@ -1,5 +1,30 @@
 package api.discord;
 
-//Contains all the functionality for this platform
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import io.github.cdimascio.dotenv.Dotenv;
+import reactor.core.publisher.Mono;
+
 public class Main {
+    /**
+     * Gets the Discord API token saved within a .env file
+     *
+     * @return API token from .env file
+     */
+    private static String envGetToken() {
+        return Dotenv.load().get("TOKEN");
+    }
+
+    /**
+     * Initializes the Discord Client
+     */
+    public static void init() {
+        String TOKEN = envGetToken();
+        DiscordClient client = DiscordClient.create(TOKEN);
+
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(Event.class, Categorize::categorizeEvent));
+        login.block();
+    }
 }
