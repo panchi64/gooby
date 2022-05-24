ec3a38396ec2f7e7d6f3e0576b6a642306f3761d
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-05-23 20:39:56 -0600

    Triggers now rely on a file for storage, and bot can respond to single word triggers

--- src/main/java/api/discord/features/TextualTriggers.java
+++ src/main/java/api/discord/features/TextualTriggers.java
@@ -1,4 +1,40 @@
 package api.discord.features;
 
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 public class TextualTriggers {
+    private Map<String, String> triggers;
+
+    /**
+     * Sets the HashMap for the class.
+     *
+     * @param triggerMap HashMap containing the Key|Value pairs desired to be searched for in messages.
+     */
+    public void setTriggers(Map<String, String> triggerMap) {
+        this.triggers = triggerMap;
+    }
+
+    /**
+     * @return Class Map containing the Key|Value pairs to be searched for in messages given to it.
+     */
+    public Map<String, String> getTriggers() {
+        return this.triggers;
+    }
+
+    public Mono<Object> respond(Map<String, String> triggerMap, MessageCreateEvent event) {
+        Message message = event.getMessage();
+        String[] listOfWords = message.getContent().split(" ");
+
+        for (String word : listOfWords) {
+            if (triggerMap.containsKey(word)) {
+                return message.getChannel().flatMap(channel -> channel.createMessage(triggerMap.get(word)));
+            }
+        }
+
+        return Mono.empty();
+    }
 }
